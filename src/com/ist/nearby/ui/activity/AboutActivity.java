f1146312190808bad1d32554e8df8e2c90d49301
b2rosado <b2rosado@gmail.com>
2014-01-02 16:59:46 +0000

    house keeping.

--- src/com/ist/nearby/ui/activity/AboutActivity.java
+++ src/com/ist/nearby/ui/activity/AboutActivity.java
@@ -1,26 +1,14 @@
 package com.ist.nearby.ui.activity;
 
-import com.example.nearby.R;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
+
+import com.example.nearby.R;
 
 public class AboutActivity extends Activity {
-	private int logoCount = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-	}
-	
-	public void logoClicked(View v){
-		if(logoCount++ == 30){
-			logoCount=0;
-			RelativeLayout screen = (RelativeLayout) findViewById(R.id.smartwatch_screen);			
-			screen.setBackground(getResources().getDrawable(R.drawable.zorro));
-		}
-			
 	}
 }
